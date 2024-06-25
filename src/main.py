a5a67badce5aeef41703ac4a52c040f2b54cba8c
johannmachemer <johannmachemer@gmail.com>
2024-06-25 16:45:42 +0000

    add method to create candidates

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from Attribute import Size
-from Rule import (Progression,Const)
+from Rule import (Progression,Const, Rule)
 from Tree import Root
 from Tree import SingleImage
 from Tree import Component
@@ -8,6 +8,22 @@ from PIL import Image, ImageDraw
 import random
 from rendering import renderRoot
 
+
+def create_candidates(root: Root, answer: SingleImage):
+
+    ruleList:list(Rule) = root.getRules();
+
+    candidates = []
+
+    for i in range(6):
+        candidate = copy.deepcopy(answer)
+
+        for rule in ruleList:
+            candidate.components[rule.component_idx].sample(rule.attr)
+
+        candidates.append(candidate);
+
+    return candidates
 
 progression = Progression("size", [0.2])
 progression.sample()
@@ -42,6 +58,14 @@ const.apply_rule(second, third)
 analog.insertAnalogie(first)
 analog.insertAnalogie(second)
 analog.insertAnalogie(third)
+
+candidates = create_candidates(analog, third)
+
+analog.insertAnswers(candidates)
+
+# analog.printLatex()
+
+# analog.saveLatex("test_with_answers")
 
 renderRoot(analog)
 
