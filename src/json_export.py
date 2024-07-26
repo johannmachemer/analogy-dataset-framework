a25c5a30dbe9868793574f58aa07d3325ec2c396
johannmachemer <johannmachemer@gmail.com>
2024-07-26 16:45:16 +0200

    refactor json export and add ids

--- src/json_export.py
+++ src/json_export.py
@@ -1,59 +1,75 @@
+import os
 from Tree import (Root,Component)
 import json
 
 
-def to_json(root:Root, filename="analogies.json"):
+def component_to_json(idx, component:Component):
+    component_dict = {}
+    component_dict["component_id"] = idx
+    component_dict["type"] = component.type.get_value()
+    component_dict["size"] = component.size.get_value()
+    component_dict["position"] = component.position.get_value()
+    
+    return component_dict
+
+
+def single_image_to_json(idx, child):
+    single_image_dict = {}
+    
+    single_image_dict["img_id"] = idx
+    single_image_dict["components"] = []
+
+
+    for (component_idx, component) in enumerate(child.components):
+        single_image_dict["components"].append(component_to_json(component_idx, component))
+
+    return single_image_dict
+    
+
+def safe_json(root:Root, analogie_id,):
     root_dict = {}
+
+    meta_dict = {}
+
+    meta_dict["id"] = analogie_id
+
+    root_dict["meta"] = meta_dict
 
     root_dict["rules"] = []
 
     for(rule_idx, rule) in enumerate(root.getRules()):
         rule_dict = {}
+        rule_dict["rule_id"] = rule_idx
+        rule_dict["rule_type"] = rule.__class__.__name__
         rule_dict["component_idx"] = rule.component_idx
         rule_dict["attr"] = rule.attr
         root_dict["rules"].append(rule_dict)
 
     analogie_list = []
 
-    for child in root.children_analogie:
-        single_image_dict = {}
+    for (idx,child) in enumerate(root.children_analogie):
         
-        single_image_dict["components"] = []
-
-        for component in child.components:
-            
-            component_dict = {}
-            component_dict["type"] = component.type.get_value()
-            component_dict["size"] = component.size.get_value()
-            component_dict["position"] = component.position.get_value()
-            single_image_dict["components"].append(component_dict)        
-        analogie_list.append(single_image_dict)
+        analogie_list.append(single_image_to_json(idx, child))
 
 
     root_dict["analogie"] = analogie_list
 
-    anser_list = []
+    answer_list = []
 
-    for child in root.children_answer:
-        single_image_dict = {}
+    for (idx,child) in enumerate(root.children_answer):
         
-        single_image_dict["components"] = []
+        answer_list.append(single_image_to_json(idx + len(root.children_analogie), child))
 
-        for component in child.components:
-            
-            component_dict = {}
-            component_dict["type"] = component.type.get_value()
-            component_dict["size"] = component.size.get_value()
-            component_dict["position"] = component.position.get_value()
-            single_image_dict["components"].append(component_dict)        
-        anser_list.append(single_image_dict)
+    root_dict["candidates"] = answer_list
+    	
+    if not os.path.isdir("data"):
+        os.mkdir("data")
+        
 
-    root_dict["candidates"] = anser_list
-
-
-
-    with open(filename, "w") as f:
+    with open(f"data/{analogie_id}/{analogie_id}.json", "w") as f:
         json.dump(root_dict, f)
-            
+
+
+
 
 
