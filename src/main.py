e473e1ef4a0d8054261e4184d2a1c149ac054a7c
johannmachemer <johannmachemer@gmail.com>
2024-07-26 16:45:26 +0200

    add method to create first sample

--- src/main.py
+++ src/main.py
@@ -6,13 +6,16 @@ from Tree import Component
 import copy
 from PIL import Image, ImageDraw
 import random
-from rendering import renderRoot
-from json_export import to_json
+from rendering import (safeRootAsCollage, safeRootAsSingleImages)
+from json_export import safe_json
+
+
+
 
 
 def create_candidates(root: Root, answer: SingleImage):
 
-    ruleList:list(Rule) = root.getRules();
+    ruleList:list(Rule) = root.getRules()
 
     candidates = []
 
@@ -20,57 +23,152 @@ def create_candidates(root: Root, answer: SingleImage):
         candidate = copy.deepcopy(answer)
 
         for rule in ruleList:
+            # just sample attr component and just attr attribute
             candidate.components[rule.component_idx].sample(rule.attr)
 
-        candidates.append(candidate);
+        candidates.append(candidate)
 
     return candidates
 
-progression = Progression("size", [0.2])
-progression.sample()
-const = Const("type")
+
+def build_first_example_sample():
+
+    for i in range(0,5):
+        
+        progression = Progression("size", [0.2])
+        progression.sample()
+        const_type = Const("type")
+        const_type.sample()
+
+        analog= Root([progression, const_type])
+
+        first = SingleImage()
+        first.insertComponent(Component())
+        first.insertComponent(Component())
+        first.sample()
+        
+        second = copy.deepcopy(first)
+        second.sample()
+        progression.apply_rule(first, second)
+        const_type.apply_rule(first, second)
 
 
-analog = Root([progression, const])
-
-first = SingleImage()
-
-first.insertComponent(Component())
-first.insertComponent(Component())
-
-first.sample()
-
-second = copy.deepcopy(first)
-
-second.sample()
-
-progression.apply_rule(first, second)
-const.apply_rule(first, second)
-
-third = copy.deepcopy(second)
-
-third.sample()
-
-progression.apply_rule(second, third)
-const.apply_rule(second, third)
+        third = copy.deepcopy(second)
+        third.sample()
+        progression.apply_rule(second, third)
+        const_type.apply_rule(second, third)
 
 
+        analog.insertAnalogie(first)
+        analog.insertAnalogie(second)
+        analog.insertAnalogie(third)
 
-analog.insertAnalogie(first)
-analog.insertAnalogie(second)
-analog.insertAnalogie(third)
+        candidates = create_candidates(analog, third)
 
-candidates = create_candidates(analog, third)
+        analog.insertAnswers(candidates)
 
-analog.insertAnswers(candidates)
+        safeRootAsSingleImages(analog, i)
+        safe_json(analog, i)
+        safeRootAsCollage(analog, i)
 
-# analog.printLatex()
 
-# analog.saveLatex("test_with_new_position")
+    for i in range(5,10):
+        
+        const_size = Const("size")
+        const_type = Const("type")
+        const_type.sample()
+        const_size.sample()
 
-# renderRoot(analog)
+        analog= Root([const_size, const_type])
 
-to_json(analog)
+        first = SingleImage()
+        first.insertComponent(Component())
+        first.insertComponent(Component())
+        first.sample()
+
+
+        
+        second = copy.deepcopy(first)
+        second.sample()
+        const_size.apply_rule(first, second)
+        const_type.apply_rule(first, second)
+
+
+
+        third = copy.deepcopy(second)
+        third.sample()
+        const_size.apply_rule(second, third)
+        const_type.apply_rule(second, third)
+
+        analog.insertAnalogie(first)
+        analog.insertAnalogie(second)
+        analog.insertAnalogie(third)
+
+        candidates = create_candidates(analog, third)
+
+        analog.insertAnswers(candidates)
+
+        safeRootAsSingleImages(analog, i)
+        safe_json(analog, i)
+        safeRootAsCollage(analog, i)
+
+
+
+
+
+
+
+    
+build_first_example_sample()
+
+
+
+
+# progression = Progression("size", [0.2])
+# progression.sample()
+# const = Const("type")
+
+
+# analog = Root([progression, const])
+
+# first = SingleImage()
+
+# first.insertComponent(Component())
+# first.insertComponent(Component())
+
+# first.sample()
+
+# second = copy.deepcopy(first)
+
+# second.sample()
+
+# progression.apply_rule(first, second)
+# const.apply_rule(first, second)
+
+# third = copy.deepcopy(second)
+
+# third.sample()
+
+# progression.apply_rule(second, third)
+# const.apply_rule(second, third)
+
+
+
+# analog.insertAnalogie(first)
+# analog.insertAnalogie(second)
+# analog.insertAnalogie(third)
+
+# candidates = create_candidates(analog, third)
+
+# analog.insertAnswers(candidates)
+
+# # analog.printLatex()
+
+# # analog.saveLatex("test_with_new_position")
+
+# # renderRoot(analog)
+
+# # safe_json(analog, 1)
 
 
 
