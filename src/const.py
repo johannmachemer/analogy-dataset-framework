4686d50f926f4c9a90e47e0917b11b72df7882e5
johannmachemer <johannmachemer@gmail.com>
2024-09-04 13:18:40 +0200

    add order for progression of corners

--- src/const.py
+++ src/const.py
@@ -19,7 +19,11 @@ FILLING_VALUES = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]
 FILLING_MIN = 0
 FILLING_MAX = len(FILLING_VALUES) -1
 
-
 # Image sizes
 SINGLE_IMAGE_WIDTH = 224
 SINGLE_IMAGE_HEIGHT = 224
+
+
+# special progression orders
+
+CORNER_ORDER = ["Circle", "Square", "Star"]
