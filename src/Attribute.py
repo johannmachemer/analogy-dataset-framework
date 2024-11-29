- create correct analogy samples - add new attribute Rotation for the Components (apply by generating image and described in json) - add new Component Group as collector of Components (unused by generating samples)

src/Attribute.py
@@ -1,8 +1,19 @@
 import numpy as np
+from enum import Enum, StrEnum
 
-from const import(SIZE_VALUES,SIZE_MAX,SIZE_MIN, TYPE_VALUES, TYPE_MAX, TYPE_MIN,SINGLE_IMAGE_HEIGHT, FILLING_MAX, FILLING_MIN, FILLING_VALUES)
+from const import (SIZE_MAX, SIZE_MIN, TYPE_VALUES, TYPE_MAX, TYPE_MIN, SINGLE_IMAGE_HEIGHT, FILLING_MAX,
+                   FILLING_MIN, ROTATION_MAX)
+from src.const import ROTATION_MIN
 
-class Attribute():
+
+class KindsOfAttributes(StrEnum):
+    SIZE = "size"
+    TYPE = "type"
+    POSITION = "position"
+    ROTATION = "rotation"
+    FILLING = "filling"
+
+class Attribute:
     """
     superclass of all attributes
 
@@ -16,11 +27,12 @@ class Attribute():
     """
 
     def __init__(self):
+        self.value = None
         pass
 
 
     
-    def sample():
+    def sample(self):
         """
         sample the value of the attribute
         """
@@ -28,9 +40,6 @@ class Attribute():
 
     def get_value(self):
         return self.value
-
-    def set_value(self, value):
-        self.value = value
         
 
 class Size(Attribute):
@@ -41,20 +50,35 @@ class Size(Attribute):
 
         Args:
             min_level (int): the minimum level of the size.
-            max_level (str): the maximum level of the size
+            max_level (int): the maximum level of the size
         """
-
+        super().__init__()
         self.min_level = min_level
         self.max_level = max_level
-        self.values = SIZE_VALUES
         
         # standard value
-        self.level = min_level
-        self.value = self.values[self.level]
+        self.value = max_level
 
     def sample(self):
-        self.level = np.random.randint(self.min_level, self.max_level+1)
-        self.value = self.values[self.level]
+        self.value = np.random.uniform(self.min_level, self.max_level)
+
+
+class Rotation(Attribute):
+
+    def __init__(self, value=0):
+        """
+        Initialize new size attribute
+        """
+
+        # standard value
+        super().__init__()
+        self.value = value
+
+    def sample(self):
+        """
+        randomize the rotation
+        """
+        self.value = np.random.randint(ROTATION_MIN, ROTATION_MAX + 1)
 
 class Type(Attribute):
    
@@ -65,8 +89,9 @@ class Type(Attribute):
 
         Args:
             min_level (int): the minimum level of the size.
-            max_level (str): the maximum level of the size
+            max_level (int): the maximum level of the size
         """
+        super().__init__()
         self.min_level = min_level
         self.max_level = max_level
         self.values = TYPE_VALUES
@@ -95,31 +120,29 @@ class Position(Attribute):
 
         Args:
             min_level (int): the minimum level of the position.
-            max_level (str): the maximum level of the position.
+            max_level (int): the maximum level of the position.
         """
-         
+        super().__init__()
         self.min_level = min_level
         self.max_level = max_level
-        
-        # standard value
-        self.level = (min_level, min_level)
-        self.value = self.level
+
+        self.value = np.array([int(max_level / 2), int(max_level /2)])
 
     
-    def sample(self, size=(0,0)):
+    def sample(self, size=0):
         """
         sample the value of the position
 
         Args:
-            size(float,float): size of the object, so its inside the borders
+            size(float): size of the object, so its inside the borders
         """
-        self.level = (np.random.randint(self.min_level, int(self.max_level - size[0]*SINGLE_IMAGE_HEIGHT)), np.random.randint(self.min_level, (self.max_level - size[1]*SINGLE_IMAGE_HEIGHT)))
-        self.value = self.level
+        width = int( size*self.max_level)
+        minimal_left_upper_corner = np.array([int(width / 2), int(width / 2)])
+        self.value = np.array([np.random.randint(self.min_level, self.max_level - width), np.random.randint(self.min_level, self.max_level - width)]) + minimal_left_upper_corner
+
 
 
 class Filling(Attribute):
-    
-
     def __init__(self, min_level = FILLING_MIN, max_level=FILLING_MAX):
         """
         Initialize new Filling attribute
@@ -128,22 +151,19 @@ class Filling(Attribute):
             min_level (int): the minimum level of the filling.
             max_level (str): the maximum level of the filling.
         """
+        super().__init__()
         self.min_level = min_level
         self.max_level = max_level
-        self.values = FILLING_VALUES
-
 
         #standard value
-        self.level = min_level
-        self.value = self.values[self.level]
+        self.value = min_level
 
    
     def sample(self):
         """
         sample the value of the filling
         """
-        self.level = np.random.randint(self.min_level, self.max_level+1)
-        self.value = self.values[self.level]
+        self.value = 1- np.random.uniform(self.min_level, self.max_level)
 
 
 

src/Rule.py
@@ -3,21 +3,21 @@ import copy
 
 from const import (TYPE_VALUES, CORNER_ORDER)
 
-class Rule():
+class Rule:
     """ Superclass of all rules """
 
     def __init__(self, attr, params=[], component_idx=0):
         """Instantiate a rule.
             Args:
                 attr (str): pre-defined name of the attribute where to apply the rule
-                params (list): a list of possible params to sample for the progressionn value
+                params (list): a list of possible params to sample for the progression value
                 component_idx (int): index of the component to apply the rule to
         """
         self.attr = attr
         self.params = params
         self.component_idx = component_idx
 
-    def apply_rule(self):
+    def apply_rule(self, image_before, new_image):
         """
         apply rule on single image
 
@@ -34,8 +34,15 @@ class Rule():
         """
         sample the value of the rule
         """
-        if(len(self.params) > 0):
+        if len(self.params) > 0:
             self.value = np.random.choice(self.params)
+        elif self.attr == "size":
+            self.value = 0.2
+        elif self.attr == "type":
+            self.value = 1
+        elif self.attr == "filling":
+            self.value = 0.2
+
 
 
 class Progression(Rule):
@@ -45,14 +52,14 @@ class Progression(Rule):
         super().__init__( attr, params, component_idx)
 
     def apply_rule(self, image_before, new_image):
-        
+
         rule_component_new = new_image.components[self.component_idx]
         rule_component_before = image_before.components[self.component_idx]
 
         #apply progression
         if self.attr == "size":
             # check if progression is possible and size will not be the whole image
-            if(rule_component_before.size.value + self.value < 1):
+            if rule_component_before.size.value + self.value < 1:
                 rule_component_new.size.value = rule_component_before.size.value + self.value
             else:
                 rule_component_new.size.value = rule_component_before.size.value
@@ -62,7 +69,7 @@ class Progression(Rule):
             rule_component_new.type.value = TYPE_VALUES[rule_component_new.type.level]
 
         if self.attr == "filling":
-            if(rule_component_before.filling.value + self.value <= 1):
+            if rule_component_before.filling.value + self.value <= 1:
                 rule_component_new.filling.value = rule_component_before.filling.value + self.value
             else:
                 rule_component_new.filling.value = rule_component_before.filling.value

src/rendering.py
@@ -1,56 +1,31 @@
-from Tree import(Root, SingleImage)
+import numpy as np
+from numpy import ndarray
+from numpy.f2py.auxfuncs import throw_error
+
+from Tree import (AnalogySample, SingleImage, Group)
 from PIL import Image, ImageDraw
 import math
 import random
 import os
-from const import (SINGLE_IMAGE_HEIGHT, SINGLE_IMAGE_WIDTH)
+from const import (SINGLE_IMAGE_HEIGHT, SINGLE_IMAGE_WIDTH, STAR_PEAKS)
 
-
-
-def render_star(draw, center, radius, points, fill, outline, width):
-    """
-    Draws a star-shaped polygon on a canvas.
-
-    Args:
-        draw: The drawing context
-        center (tuple): The (x, y) coordinates of the star's center.
-        radius (float): The radius of the star (distance from center to outermost points).
-        points (int): The number of points (or spikes) the star should have.
-        fill: The fill color of the star.
-        outline: The outline color of the star.
-        width (int): The width of the star's outline.
-    """
-    angle = math.pi / points
-    rotation_offset = math.pi / 2
-
-    coords = []
-    for i in range(2 * points):
-        r = radius if i % 2 == 0 else radius / 2
-        x = center[0] + r * math.cos(i * angle + rotation_offset)
-        y = center[1] - r * math.sin(i * angle + rotation_offset)
-        coords.append((x, y))
-
-    draw.polygon(coords, fill=fill, outline=outline, width=width)
-
-
-
-def safeRootAsSingleImages(root,id):
+def safe_root_as_single_images(root, id):
     """
     Saves a Root as single images
 
     Args:
-        root (Root): root to save
+        root (AnalogySample): root to save
         id (int): id of the analogy
     """
-    assert isinstance(root, Root)
+    assert isinstance(root, AnalogySample)
 
     images = []
 
-    for single_image in root.children_analogie:
-        images.append(renderSingleImage(single_image))
+    for single_image in root.analogy:
+        images.append(render_single_image(single_image))
 
-    for single_image in root.children_answer:
-        images.append(renderSingleImage(single_image))
+    for single_image in root.candidates:
+        images.append(render_single_image(single_image))
 
     if not os.path.isdir("data"):
         os.mkdir("data")
@@ -66,28 +41,28 @@ def safeRootAsSingleImages(root,id):
 
     
 
-def safeRootAsCollage(root, id):
+def safe_root_as_collage(root, id):
     """
     Saves a Root as a collage
 
     Args:
-        root (Root): root to save
+        root (AnalogySample): root to save
         id (int): id of the analogy
     """
 
-    assert isinstance(root, Root)
+    assert isinstance(root, AnalogySample)
 
     images = []
 
-    for single_image in root.children_analogie:
-        images.append(renderSingleImage(single_image))
+    for single_image in root.analogy:
+        images.append(render_single_image(single_image))
 
     analog_image = concatenate_images_horizontally(images)
 
     images = []
 
-    for single_image in root.children_answer:
-        images.append(renderSingleImage(single_image))
+    for single_image in root.candidates:
+        images.append(render_single_image(single_image))
 
     answer_images_1 = concatenate_images_horizontally(images[0:3])
 
@@ -101,8 +76,106 @@ def safeRootAsCollage(root, id):
     all_images.save(f"data/{id}/{id}-collage.png")
 
 
+def determine_all_rendering_objects(components_list, super_width = SINGLE_IMAGE_WIDTH, super_fill = 1):
+    result = []
+    for component in components_list:
+        if isinstance(component, Group):
+            subcomponents = determine_all_rendering_objects(component.components, super_width * component.size.get_value(),
+                                                            super_fill * component.filling.get_value())
+            for subcomponent in subcomponents:
+                subcomponent.translate_and_rotate(component.position.value, component.rotation.value)
+            result.extend(subcomponents)
+        else:
+            if component.type.get_value() == "Square":
+                rendering_object = Square(component, super_width, super_fill)
+            elif component.type.get_value() == "Circle":
+                rendering_object = Circle(component, super_width, super_fill)
+            else:
+                rendering_object = Star(component, super_width, super_fill)
+            result.append(rendering_object)
+    return result
 
-def renderSingleImage(single_image):
+
+def translate_and_rotate(points, center, angle):
+        theta = angle / 180 * np.pi
+        c, s = np.cos(angle), np.sin(theta)
+        rotation_matrix = np.array(((c, -s), (s, c)))
+        return np.matmul(points, rotation_matrix) + center
+
+def filling(component, super_fill):
+    value = component.filling.get_value()
+    fill_color = int(255 * value * super_fill)
+    return fill_color, fill_color, fill_color
+
+def convert_coordinates(coordinates:ndarray[:2]) -> list[tuple[float, float]]:
+    return [ (x,y) for x,y in coordinates]
+
+class RenderingObject:
+
+    def __init__(self, position, width, angle, fill):
+        w = width / 2
+        self.fill = fill
+        self.boundingBox = translate_and_rotate(
+            np.array([[-w, -w], [w, -w], [w, w], [-w, w]]),
+            position,
+            angle)
+
+    def translate_and_rotate(self, center, angle):
+        self.boundingBox =  translate_and_rotate(self.boundingBox, center, angle)
+
+    def draw(self, canvas):
+        raise NotImplementedError()
+
+class Circle(RenderingObject):
+
+    def __init__(self, component, width, super_fill):
+        circle_diameter = int(width * component.size.get_value())
+        super().__init__(component.position.value, circle_diameter, 0, filling(component, super_fill))
+
+    def translate_and_rotate(self, center, angle):
+        self.boundingBox =  super().translate_and_rotate(center, 0)
+
+    def draw(self, canvas):
+        canvas.ellipse(convert_coordinates([self.boundingBox[0], self.boundingBox[2]]), outline="black", width=2, fill=self.fill)
+
+class Square(RenderingObject):
+
+    def __init__(self, component, width, super_fill):
+        rect_width = int(width * component.size.get_value())
+        super().__init__(component.position.value, rect_width, component.rotation.value % 90, filling(component, super_fill))
+
+    def draw(self, canvas):
+        canvas.polygon(convert_coordinates(self.boundingBox), fill=self.fill, outline="black", width=2)
+
+    def translate_and_rotate(self, center, angle):
+        super().translate_and_rotate(center, 0)
+
+class Star(RenderingObject):
+
+    def __init__(self, component, width, super_fill):
+        center = component.position.get_value()
+        radius = int(width * component.size.get_value() / 2)
+        angle = math.pi / STAR_PEAKS
+        rotation_offset = math.pi / 2
+
+        self.coordinates = np.empty((0, 2))
+        for i in range(2 * STAR_PEAKS):
+            r = radius if i % 2 == 0 else radius / 2
+            x = center[0] + r * math.cos(i * angle + rotation_offset)
+            y = center[1] + r * math.sin(i * angle + rotation_offset)
+            self.coordinates = np.concatenate((self.coordinates, [(x, y)]))
+        super().__init__(component.position.value, radius * 2, component.rotation.value, filling(component, super_fill))
+
+    def translate_and_rotate(self, center, angle):
+        super().translate_and_rotate(center, angle % (360 / STAR_PEAKS))
+        self.coordinates = translate_and_rotate(self.coordinates, center, angle % (360 / STAR_PEAKS))
+
+    def draw(self, canvas):
+        canvas.polygon(convert_coordinates(self.coordinates), fill=self.fill, outline="black", width=2)
+
+
+
+def render_single_image(single_image):
     """
     Render a single image
 
@@ -112,47 +185,16 @@ def renderSingleImage(single_image):
     Returns:
         img (Image): The rendered Image
     """
-
-
     assert isinstance(single_image, SingleImage)
 
     img = Image.new("RGB", (SINGLE_IMAGE_WIDTH, SINGLE_IMAGE_HEIGHT), "white")
 
     draw = ImageDraw.Draw(img)
 
-    for component in single_image.components:
+    rendering_objects = determine_all_rendering_objects(single_image.components)
 
-        filling = component.filling.get_value()
-        fill_color = int(255 * (1-filling)) 
-        fill = (fill_color, fill_color, fill_color)
-
-        if component.type.get_value() == "Square":
-
-            rect_width = int(SINGLE_IMAGE_HEIGHT * component.size.get_value())
-            x0 = component.position.get_value()[0]
-            y0 = component.position.get_value()[1]
-
-            x1 = x0 + rect_width
-            y1 = y0 + rect_width
-            draw.rectangle([x0, y0, x1, y1], outline="black", width= 2, fill=fill)
-        elif component.type.get_value() == "Circle":
-
-            circle_diameter = int(min(SINGLE_IMAGE_HEIGHT, SINGLE_IMAGE_WIDTH) * component.size.get_value())
-
-            
-
-            x0 = component.position.get_value()[0]
-            y0 = component.position.get_value()[1]
-            x1 = x0 + circle_diameter
-            y1 = y0 + circle_diameter
-
-            draw.ellipse([x0, y0, x1, y1], outline="black", width= 2, fill=fill)
-
-        elif component.type.get_value() == "Star":
-            center = component.position.get_value()
-            radius = int(min(SINGLE_IMAGE_HEIGHT, SINGLE_IMAGE_WIDTH) * component.size.get_value() / 2)
-            render_star(draw, center, radius, 5, fill, "black", 2)
-    
+    for rendering_object in rendering_objects:
+        rendering_object.draw(draw)
 
     return img
 
@@ -185,8 +227,6 @@ def concatenate_images_horizontally(images):
             current_x += 5
 
     return new_image
-
-
 
 def concatenate_images_vertical(images):
     """