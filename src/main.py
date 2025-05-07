53b9d88123f86958387b44f31d766571e0fedc94
Gesina Schwalbe <gesina.schwalbe@uni-luebeck.de>
2025-05-07 11:44:51 +0200

    generation of 10000 samples with progress bar

--- src/main.py
+++ src/main.py
@@ -4,10 +4,11 @@ from src.SampleGenerator import create_analogy_sample, validate_image
 from src.const import NUMBER_OF_SAMPLES, MIN_COMPONENTS, MAX_COMPONENTS
 from src.json_importer import import_json, import_json_image
 from src.raven_format_exporter import write_sample
+from tqdm import tqdm
 
 
 def build_random_samples():
-    for id in range(0, NUMBER_OF_SAMPLES):
+    for id in tqdm(range(0, NUMBER_OF_SAMPLES), desc="Samples generated"):
         analogy = create_analogy_sample()
         safe_root_as_single_images(analogy, id)
         safe_json(analogy, id)
